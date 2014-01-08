741b49b4b8d1c4065a8c0379a6f5101c9f5d1abd
Nick Albino <nick.albino@gmail.com>
2014-01-08 14:30:26 -0800

    8.16

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,7 +14,20 @@ describe "Authentication" do
   end
 
   describe "signin" do
-    before { visit sign_path }
+
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
@@ -28,13 +41,6 @@ describe "Authentication" do
       it {should have_link('Profile', href: user_path(user)) }
       it {should have_link('Sign out', href: signout_path) }
       it {should_not have_link('Sign in', href: signin_path) }
-    end
-
-    describe "with invalid information" do
-      before { click_button "Sign in" }
-
-      it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
     end
   end
 end
