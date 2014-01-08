55bbb24c89560d5f8da35188a24c47c50135b370
Nick Albino <nick.albino@gmail.com>
2014-01-07 21:22:32 -0800

    weird corrections back to tutorial 6.3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,29 +3,24 @@ require 'spec_helper'
 describe User do
 
   before do
-
-  @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  it { should be_valid}
+
+  it { should be_valid }
 
   describe "when name is not present" do
-    before { @user.name = " "}
-    it { should_not be_valid}
-  end
-
-  describe "when email is not present" do
-    before { @user.email= " " }
+    before { @user.name = " " }
     it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it {should_not be_valid}
+    describe "when email is not present" do
+      before { @user.email = " " }
+      it { should_not be_valid }
   end
 
   describe "when email format is invalid" do
@@ -55,8 +50,10 @@ describe User do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-    it {should_not be_valid}
+
+    it { should_not be_valid }
   end
 
 
-end
+
+  end
