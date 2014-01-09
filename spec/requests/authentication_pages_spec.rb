e4bf108861b9ce7cf338a12d891d660e69902156
Nick Albino <nick.albino@gmail.com>
2014-01-09 11:17:50 -0800

    9.2.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,6 +5,26 @@ describe "Authentication" do
 
   subject { page }
 
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it {should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+  end
   describe "signin page" do
     before { visit signin_path }
 
