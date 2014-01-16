7fac76ee5703e3b0c3f13730b9ca2a85472645dd
Nick Albino <nick.albino@gmail.com>
2014-01-15 18:59:51 -0800

    10.5

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -2,7 +2,22 @@ require 'spec_helper'
 
 describe "Micropost pages" do
 
+
+
   subject { page }
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
+
 
   let(:user) { FactoryGirl.create(:user) }
   before { sign_in user }
